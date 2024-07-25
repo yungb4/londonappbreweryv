c8cde62f66d2abdd6812e17db368a184eac25708
yungb4 <zhushe62062@126.com>
2024-07-25 10:32:18 +0800

    修复一些严重的bug

--- enviroment/__init__.py
+++ enviroment/__init__.py
@@ -2,7 +2,9 @@ import threading as _threading
 import time
 from queue import LifoQueue as _LifoQueue
 
-from PIL import Image, ImageTk, ImageFont as _Image, _ImageFont, ImageDraw
+from PIL import Image as _Image,\
+    ImageFont as _ImageFont,\
+    ImageDraw as _ImageDraw
 from system import threadpool as _threadpool
 from .drivers import epd2in9_V2, icnt86
 from .touchscreen import Clicked as _Clicked, \
@@ -14,9 +16,9 @@ import os as _os
 
 class Images:
     def __init__(self):
-        self.app_control = Image.open("resources/images/app_control.jpg")
-        self.None18px = Image.open("resources/images/None18px.jpg")
-        self.None20px = Image.open("resources/images/None20px.jpg")
+        self.app_control = _Image.open("resources/images/app_control.jpg")
+        self.None18px = _Image.open("resources/images/None18px.jpg")
+        self.None20px = _Image.open("resources/images/None20px.jpg")
 
 
 class Env:
