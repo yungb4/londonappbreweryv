8741822c8f56eabc7c94933963a7dcff6a9d9ff9
yungb4 <zhushe62062@126.com>
2025-04-07 17:28:49 +0800

    应用抽屉完工

--- applications/应用抽屉/main.py
+++ applications/应用抽屉/main.py
@@ -8,12 +8,14 @@ from PIL import Image
 class ApplicationList(Pages.ListPage):
     def __init__(self, book):
         super().__init__(book, "应用抽屉", [])
+
+    def update_app_list(self):
         for app in self.book.base.env.apps:
             if app == "应用抽屉":
                 continue
             self.items.append(app)
-            self.icons.append(self.book.env.apps[app].icon)
-            self.funcs.append(lambda: self.book.env.open_app(app))
+            self.icons.append(self.book.base.env.apps[app].icon)
+            self.funcs.append(lambda: self.book.base.env.open_app(app))
 
 
 class MainBook(Book):
@@ -31,3 +33,6 @@ class Application(AppBase):
         self.icon = Image.open("applications/应用抽屉/icon.png")
         self.now_book = "main"
         self.title = self.name
+
+    def update_app_list(self):
+        self.Book.Page.update_app_list()
